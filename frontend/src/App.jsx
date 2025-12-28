36824e688ac797f066e309232177049c7dac05e7
hridik-suresh <iamhridiksuresh@gmail.com>
2025-12-28 12:01:45 +0530

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Added package.json with dependencies for React, Vite, and Tailwind CSS.
    - Created main application component (App.jsx) with a simple "Hello World!" message styled using Tailwind CSS.
    - Included an SVG asset for React logo.
    - Set up global CSS file (index.css) to import Tailwind CSS styles.
    - Configured main entry point (main.jsx) to render the App component.
    - Created Vite configuration file (vite.config.js) to integrate React and Tailwind CSS plugins.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-lg text-blue-600 p-5  border-2 rounded-lg flex justify-center items-center ">Hello World!</h1>
+    </>
+  )
+}
+
+export default App
