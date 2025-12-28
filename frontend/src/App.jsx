1d964fb6ccdd00bfc07388fb65ada9790dd9b6ab
hridik-suresh <iamhridiksuresh@gmail.com>
2025-12-28 14:35:26 +0530

    Add routing and create page components for Home, About, Contact, Cart, Collection, Login, Orders, PlaceOrder, and Product.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,32 @@
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Cart from "./pages/Cart"
+import Collection from "./pages/Collection"
+import Login from "./pages/Login"
+import Orders from "./pages/Orders"
+import PlaceOrder from "./pages/PlaceOrder"
+import Product from "./pages/Product"
+
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-lg text-blue-600 p-5  border-2 rounded-lg flex justify-center items-center ">Hello World!</h1>
-    </>
+    <div className="px-4 sm:px-[5vw] md:px-[7wv] lg:px-[9vw]">
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/product/:productId" element={<Product />} />
+      </Routes>
+    </div>
   )
 }
 
