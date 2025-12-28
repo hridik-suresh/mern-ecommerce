e07dca3a14b5e622a0ce08d4d19f011f3af87040
hridik-suresh <iamhridiksuresh@gmail.com>
2025-12-28 18:30:35 +0530

    Navbar component is completed.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,13 +8,14 @@ import Login from "./pages/Login"
 import Orders from "./pages/Orders"
 import PlaceOrder from "./pages/PlaceOrder"
 import Product from "./pages/Product"
+import Navbar from "./components/Navbar"
 
 
 function App() {
 
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7wv] lg:px-[9vw]">
-
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
