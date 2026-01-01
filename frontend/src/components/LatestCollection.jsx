c0d9809e37947793770d573d3d4a0623b498dd42
hridik-suresh <iamhridiksuresh@gmail.com>
2026-01-01 19:45:56 +0530

    Add LatestCollection, Title, and ProductItem components for displaying latest products.

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,11 +1,40 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/shopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
 
 function LatestCollection() {
   const { products } = useContext(ShopContext);
-  console.log(products[0]);
+  const [latestProducts, setLatestProducts] = useState([]);
 
-  return <div className=""></div>;
+  useEffect(() => {
+    setLatestProducts(products.slice(0, 10)); //Only 10 products
+  }, [products]);
+
+  return (
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"LATEST"} text2={"COLLECTIONS"} />
+        <p className="w-3/4 m-auto text-sm sm:text-sm md:text-base text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur,
+          incidunt!
+        </p>
+      </div>
+
+      {/* Rendering Products */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-y-6 ">
+        {latestProducts.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            name={item.name}
+            image={item.image}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default LatestCollection;
