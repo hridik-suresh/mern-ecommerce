6c11cf63b70154c999a047c000bc9c5b94643014
hridik-suresh <iamhridiksuresh@gmail.com>
2026-01-06 21:25:04 +0530

    The functionality to increase the quantity of cart item by increasing the  number in the Cart page is added.

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -4,7 +4,8 @@ import Title from "../components/Title";
 import { assets } from "../assets/assets";
 
 const Cart = () => {
-  const { products, currency, cartItems, updateQuantity } = useContext(ShopContext);
+  const { products, currency, cartItems, updateQuantity } =
+    useContext(ShopContext);
 
   const [cartData, setCartData] = useState([]);
 
@@ -62,11 +63,20 @@ const Cart = () => {
               <input
                 type="number"
                 min={1}
+                onChange={(e) => {
+                  e.target.value === "" || e.target.value === "0"
+                    ? null
+                    : updateQuantity(
+                        item._id,
+                        item.size,
+                        Number(e.target.value)
+                      );
+                }}
                 defaultValue={item.quantity}
                 className="border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1"
               />
               <img
-              onClick={()=>updateQuantity(item._id, item.size, 0)}
+                onClick={() => updateQuantity(item._id, item.size, 0)}
                 src={assets.bin_icon}
                 className="w-4 mr-4 sm:w-5 cursor-pointer"
                 alt=""
