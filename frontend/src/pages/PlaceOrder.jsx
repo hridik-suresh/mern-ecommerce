a27ce95fc071c59e84064913fa7082b746ce75d4
hridik-suresh <iamhridiksuresh@gmail.com>
2026-01-07 14:23:40 +0530

    Basic layout of the PlaceOrder page is completed.

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,8 +1,83 @@
+import Title from "../components/Title";
+import CartTotal from "../components/CartTotal";
 
 const PlaceOrder = () => {
   return (
-    <div>PlaceOrder</div>
-  )
-}
+    <div className="flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t">
+      {/* ------------Left Side---------- */}
+      <div className="flex flex-col gap-4 w-full sm:max-w-120">
+        <div className="text-xl sm:text-2xl my-3 ">
+          <Title text1={"DELIVERY"} text2={"INFORMATION"} />
+        </div>
+        <div className="flex gap-3">
+          <input
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="First name"
+          />
+          <input
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="Last name"
+          />
+        </div>
+        <input
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          type="email"
+          placeholder="Email address"
+        />
+        <input
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          type="text"
+          placeholder="Street"
+        />
+        <div className="flex gap-3">
+          <input
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="City"
+          />
+          <input
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="State"
+          />
+        </div>
+        <div className="flex gap-3">
+          <input
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="number"
+            placeholder="Zipcode"
+          />
+          <input
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+            type="text"
+            placeholder="Country"
+          />
+        </div>
+        <input
+          className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          type="number"
+          placeholder="Phone"
+        />
+      </div>
 
-export default PlaceOrder
+      {/* -----------Right Side------------ */}
+      <div className="mt-8">
+        <div className="mt-8 min-w-80">
+          <CartTotal />
+        </div>
+
+        <div className="mt-12">
+          <Title text1={'PAYMENT'} text2={'METHOD'}/>
+          {/* -------Payment Method Selection---------- */}
+          <div className="flex gap-3 flex-col lg:flex-row">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlaceOrder;
