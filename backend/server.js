f754cff79de0eb1620a73a26ba8f94f38839ed8f
hridik-suresh <iamhridiksuresh@gmail.com>
2026-01-09 16:54:15 +0530

    Connection to the mongoDB cloud storage is established successfully.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/mongodb");
 require("dotenv").config();
 
+//App config
 const app = express();
-
 const port = process.env.PORT || 4000;
+connectDB();
 
 //middlewares
 app.use(express.json());
