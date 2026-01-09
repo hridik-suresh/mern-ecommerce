e400ef3322bd5e3b9b95c6ff9baf312b070ebc2f
hridik-suresh <iamhridiksuresh@gmail.com>
2026-01-10 00:35:19 +0530

    Logic for the user to register is  created and tested useing postman successfully.
    Logic to validate the email and password using "validator", make use of "bcrypt" for hashing the password and "jsonwebtoken" for generating the tokens.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,81 @@
-//For user Login
-const loginUser = async (req, res) => {};
+const asyncHandler = require("express-async-handler");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
-//For user Registration
-const registerUser = async (req, res) => {
-    res.json({ message: "User registered successfully" });
+
+
+//Function to create JWT token
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 };
 
-//For admin Login
-const adminLogin = async (req, res) => {};
+//@desc   Login user
+//@route  POST /api/user/login
+//@access Public
+const loginUser = asyncHandler(async (req, res) => {});
+
+//@desc   Register user
+//@route  POST /api/user/register
+//@access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  //checking user already exist
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    return res
+      .status(400)
+      .json({ message: "User already exists", success: false });
+  }
+
+  //validating email format and strong password
+  if (!validator.isEmail(email)) {
+    return res
+      .status(400)
+      .json({ message: "Invalid email format", success: false });
+  }
+
+  if (password.length < 8) {
+    return res
+      .status(400)
+      .json({
+        message: "Password must be at least 8 characters long",
+        success: false,
+      });
+  }
+
+  //hashing user password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  const token = createToken(user._id);
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: token,
+      success: true,
+    });
+  } else {
+    res.status(400).json({ message: "Invalid user data", success: false });
+  }
+});
+
+//@desc   Login user as admin
+//@route  POST /api/user/admin
+//@access Public
+const adminLogin = asyncHandler(async (req, res) => {});
 
 module.exports = {
   loginUser,
