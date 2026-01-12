05732a86d1dda8da65d7de97801dc7a46e62dda3
hridik-suresh <iamhridiksuresh@gmail.com>
2026-01-12 16:55:50 +0530

    Successfully added the routes of the admin panel in the App.jsx file.

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,9 @@
-import NavBar from "./components/NavBar";
 import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Orders";
 
 const App = () => {
   return (
@@ -9,6 +13,13 @@ const App = () => {
         <hr />
         <div className="flex w-full">
           <Sidebar />
+          <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+            <Routes>
+              <Route path="/add" element={<Add />}></Route>
+              <Route path="/list" element={<List />}></Route>
+              <Route path="/orders" element={<Orders />}></Route>
+            </Routes>
+          </div>
         </div>
       </>
     </div>
