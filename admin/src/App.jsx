1e5f95704dfa6ebf6a669083ebfadf121bc754d8
hridik-suresh <iamhridiksuresh@gmail.com>
2026-01-13 16:42:53 +0530

    Admin login page is completed.

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -4,24 +4,33 @@ import NavBar from "./components/NavBar";
 import Add from "./pages/Add";
 import List from "./pages/List";
 import Orders from "./pages/Orders";
+import { useState } from "react";
+import Login from "./components/Login";
+
 
 const App = () => {
+  const [token, setToken] = useState("");
+
   return (
     <div className="bg-gray-50 min-h-screen">
-      <>
-        <NavBar />
-        <hr />
-        <div className="flex w-full">
-          <Sidebar />
-          <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
-            <Routes>
-              <Route path="/add" element={<Add />}></Route>
-              <Route path="/list" element={<List />}></Route>
-              <Route path="/orders" element={<Orders />}></Route>
-            </Routes>
+      {token === "" ? (
+        <Login />
+      ) : (
+        <>
+          <NavBar />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<Add />}></Route>
+                <Route path="/list" element={<List />}></Route>
+                <Route path="/orders" element={<Orders />}></Route>
+              </Routes>
+            </div>
           </div>
-        </div>
-      </>
+        </>
+      )}
     </div>
   );
 };
