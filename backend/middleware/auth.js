085d28e3a5d83ac29193d2ba2acb7edcc6a5f043
hridik-suresh <iamhridiksuresh@gmail.com>
2026-01-15 19:04:15 +0530

    authUser middleware is created to authorise the user.

---
+++ backend/middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+
+const authUser = asyncHandler(async (req, resizeBy, next) => {
+  const { token } = req.headers;
+
+  if (!token) {
+    return res.json({ success: false, message: "Not Authorized, Login Again" });
+  }
+
+  try {
+    const token_decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.body.userId = token_decoded.id;
+    next();
+  } catch (err) {
+    console.log(err);
+    res.json({ success: false, message: err.message });
+  }
+});
+
+module.exports = authUser;
