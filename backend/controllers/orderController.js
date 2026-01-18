bd9e56c7b63d7f6326e98a5473906e3b0a44975b
hridik-suresh <iamhridiksuresh@gmail.com>
2026-01-18 17:20:50 +0530

    The logic for the controller placeOrder to place order using COD method is created.

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,32 @@
+const Order = require("../models/Order.js");
+const User = require("../models/User");
+
 //Place order using COD method
-const placeOrder = async (req, res) => {};
+const placeOrder = async (req, res) => {
+  try {
+    const { userId, items, amount, address } = req.body;
+
+    const orderData = {
+      userId,
+      items,
+      address,
+      amount,
+      paymentMethod: "COD",
+      payment: false,
+      date: Date.now(),
+    };
+    const newOrder = new Order(orderData);
+    await newOrder.save();
+
+    //Clearing the current cart data after placing order
+    await User.findByIdAndUpdate(userId, { cartData: {} });
+
+    res.json({ success: true, message: "Order Placed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 //Place order using Stripe method
 const placeOrderStripe = async (req, res) => {};
