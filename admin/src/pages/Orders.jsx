bd929203524f39c162a9678ba6cb79362c8f4dc3
hridik-suresh <iamhridiksuresh@gmail.com>
2026-01-24 15:39:23 +0530

    Implement order fetching functionality in Orders component in the  Admin Panel.

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -1,8 +1,36 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-const Orders = () => {
-  return (
-    <div>Orders</div>
-  )
-}
+const Orders = ({ token }) => {
+  const [orders, setOrders] = useState([]);
 
-export default Orders
+  const fetchAllOrders = async () => {
+    if (!token) return null;
+
+    try {
+      const response = await axios.post(
+        import.meta.env.VITE_BACKEND_URL + "/api/order/list",
+        {},
+        { headers: { token } },
+      );
+      if (response.data.success) {
+        setOrders(response.data.orders);
+      }else{
+        toast.error(response.data.message); // Show error
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to fetch orders");
+    }
+  };
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, [token]);
+
+  return <div>Orders</div>;
+};
+
+export default Orders;
