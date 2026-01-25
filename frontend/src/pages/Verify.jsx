1c56a26725deb9722328b4c4fb9a81fef1c8eaaf
hridik-suresh <iamhridiksuresh@gmail.com>
2026-01-25 23:58:46 +0530

    Implement payment verification logic in Verify component.

--- frontend/src/pages/Verify.jsx
+++ frontend/src/pages/Verify.jsx
@@ -1,7 +1,47 @@
-
+import { useContext, useEffect } from "react";
+import { ShopContext } from "../context/shopContext";
+import { useSearchParams } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 function Verify() {
-  return <div>Verify Page</div>;
+  const { navigate, token, setCartItems, backendUrl } = useContext(ShopContext);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const success = searchParams.get("success");
+  const orderId = searchParams.get("orderId");
+  const userId = searchParams.get("userId");
+
+  const verifyPayment = async () => {
+    try {
+      if (!token) {
+        return null;
+      }
+
+      const response = await axios.post(
+        backendUrl + "/api/order/verifyStripe",
+        { orderId, success, userId },
+        { headers: { token } },
+      );
+
+      if (response.data.success) {
+        setCartItems({});
+        navigate("/orders");
+      } else {
+        navigate("/cart");
+        toast.error("Payment failed. Please try again.");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    verifyPayment();
+  }, [token]);
+
+  return <div></div>;
 }
 
 export default Verify;
