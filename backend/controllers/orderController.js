8d5cfdd3db30ea239a884f0861f68bcfed449cd0
hridik-suresh <iamhridiksuresh@gmail.com>
2026-01-25 15:47:12 +0530

    STRIPE: Implement Stripe payment processing in placeOrderStripe function.

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,13 @@
 const Order = require("../models/Order.js");
 const User = require("../models/User");
+const Stripe = require("stripe");
+
+//global variables
+const currency = "inr";
+const deliveryCharges = 10;
+
+//gateway initialization
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 //Place order using COD method
 const placeOrder = async (req, res) => {
@@ -29,7 +37,59 @@ const placeOrder = async (req, res) => {
 };
 
 //Place order using Stripe method
-const placeOrderStripe = async (req, res) => {};
+const placeOrderStripe = async (req, res) => {
+  try {
+    const { userId, items, amount, address } = req.body;
+    const { origin } = req.headers;
+
+    const orderData = {
+      userId,
+      items,
+      address,
+      amount,
+      paymentMethod: "Stripe",
+      payment: false,
+      date: Date.now(),
+    };
+
+    const newOrder = new Order(orderData);
+    await newOrder.save();
+
+    const line_items = items.map((item) => ({
+      price_data: {
+        currency: currency,
+        product_data: {
+          name: item.name,
+        },
+        unit_amount: item.price * 100,
+      },
+      quantity: item.quantity,
+    }));
+
+    line_items.push({
+      price_data: {
+        currency: currency,
+        product_data: {
+          name: 'Delivery Charges',
+        },
+        unit_amount: deliveryCharges * 100,
+      },
+      quantity: 1,
+    });
+
+    const session = await stripe.checkout.sessions.create({
+      line_items: line_items,
+      mode: "payment",
+      success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+    });
+
+    res.json({ success: true, session_url: session.url });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 //Place order using RazorPay method
 const placeOrderRazorpay = async (req, res) => {};
@@ -37,10 +97,8 @@ const placeOrderRazorpay = async (req, res) => {};
 //All orders data for admin panel
 const allOrders = async (req, res) => {
   try {
-    
     const orders = await Order.find({});
     res.json({ success: true, orders });
-
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
